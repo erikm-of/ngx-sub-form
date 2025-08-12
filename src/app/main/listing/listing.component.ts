aecc6a01932036b6be36ee526a34cd91ac160514
Maxime Robert <maxime@cloudnc.com>
2025-08-12 14:28:15 +0200

    refactor: follow up from ophirKatz's MR

--- src/app/main/listing/listing.component.ts
+++ src/app/main/listing/listing.component.ts
@@ -45,10 +45,7 @@ export class ListingComponent {
     };
   }
 
-  public listingOutput?: OneListing;
   public upsertListing(listing: OneListing): void {
-    this.listingOutput = listing;
-    console.log('upsertListing', listing);
     this.listingService.upsertListing(listing);
   }
 }
